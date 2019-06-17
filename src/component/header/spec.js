768684cd3e5bb0d4ce0484227960d266040ca140
ruobing guan <ruobingguan@ruobings-MacBook-Pro.local>
2019-06-16 23:45:09 -0700

    initial commit

---
+++ src/component/header/spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './header';
+
+
+describe('Header Component', () => {
+    
+    it('It should render without errors', () => {
+        expect(1).toBe(3);
+    })
+
+})
